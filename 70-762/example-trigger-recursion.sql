b21fd393001aa8aeb836405786bbcf2bb75ffdb4
Leonid Petrov1 <leonid_petrov1@epam.com>
2020-02-27 22:37:00 +0400

    Add trigger recursion example

---
+++ 70-762/example-trigger-recursion.sql
@@ -0,0 +1,22 @@
+﻿create table examples.users (id int not null, username nvarchar(255) not null);
+go
+create trigger examples.TriggerUsers
+on examples.users
+after UPDATE
+as
+begin
+	RAISERROR ('trigger fired', 0, 0) with nowait;
+	declare @id int = (select top (1) id from inserted order by id desc);
+	update examples.users
+	set username = UPPER(username)
+	where id = @id;
+end;
+go
+
+insert into examples.users (id, username) values (1, 'leonid');
+
+update examples.users
+set username = 'leonid2'
+
+select * from examples.users;
+
